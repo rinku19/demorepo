7fce773b1da7060f2e4fd184cfac013821eca09c
rinku012 <rinkuyadav03972@gmail.com>
2024-03-01 00:39:51 +0530

    first commit

---
+++ springboottest/src/main/java/com/rays/common/ApplicationExceptionHandlerCtl.java
@@ -0,0 +1,23 @@
+package com.rays.common;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * Handles application propagated exceptions
+ * 
+ * @author Rinku
+ *
+ */
+@ControllerAdvice
+public class ApplicationExceptionHandlerCtl {
+	
+	@ExceptionHandler(value = RuntimeException.class)
+	public ORSResponse handleRuntimeException(RuntimeException e){
+		ORSResponse res = new ORSResponse(false);
+		res.addMessage(e.getMessage());
+		return res;
+		//kjfkdfhdjfh
+	}
+
+}
